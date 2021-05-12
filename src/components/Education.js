e05e9a3ae5ac6ad269a31be901935594da988647
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-05-12 17:36:22 -0400

    Project initialized with boilerplate removed

---
+++ src/components/Education.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Education() {
+  return (
+    <div class="education">
+      <h1>Education</h1>
+      <div>
+        <form>
+          <div class="card">
+            <header>
+              <h3>Education #</h3>
+              <i>delete_icon_here</i>
+            </header>
+          </div>
+          <button class="add-new-card"></button>
+          <button class="previous-page"></button>
+          <button class="next-page"></button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Education;
