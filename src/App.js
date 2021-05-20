51fde9b1fb769105fe6ea78e39612c25ce527179
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-05-20 18:24:58 -0400

    style: change primary-color and add secondary-color. feat: display nav buttons based on current section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./styles/index.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+
 library.add(fab, fas);
 
 function App() {
--- src/components/ResumeButtons.js
+++ src/components/ResumeButtons.js
@@ -12,19 +12,49 @@ function ResumeButtons({ sectionTitle, navigateSections, newComponentData }) {
       );
     }
   };
+
+  let NavBtns = () => {
+    switch (sectionTitle) {
+      case "Personal":
+        return (
+          <div className="button-container">
+            <button className="btn btn-big" name="Next" onClick={navigateSections}>
+              Next
+            </button>
+          </div>
+        );
+      case "Education":
+        return (
+          <div className="button-container">
+            <button className="btn" name="Previous" onClick={navigateSections}>
+              Previous
+            </button>
+            <button className="btn" name="Next" onClick={navigateSections}>
+              Next
+            </button>
+          </div>
+        );
+      case "Experience":
+        return (
+          <div className="button-container">
+            <button className="btn" name="Previous" onClick={navigateSections}>
+              Previous
+            </button>
+            <button className="btn submit" name="Submit" onClick={navigateSections}>
+              Submit
+            </button>
+          </div>
+        );
+      default:
+        break;
+    }
+  };
   return (
     <div>
       <div className="button-container">
         <AddNewBtn />
       </div>
-      <div className="button-container">
-        <button className="btn" name="Previous" onClick={navigateSections}>
-          Previous
-        </button>
-        <button className="btn" name="Next" onClick={navigateSections}>
-          Next
-        </button>
-      </div>
+      <NavBtns />
     </div>
   );
 }
