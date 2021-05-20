9838915d8d3455201a2485e644345d6253076287
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-05-20 19:35:10 -0400

    remove: count for personal title section

--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -24,7 +24,7 @@ function FormHeader({ sectionTitle, formIndex }) {
   return (
     <header className="resume-card__header">
       <h3 className="resume-card__h3">
-        {sectionTitle} {formIndex + 1}
+        {sectionTitle} {sectionTitle === "Personal" ? "" : formIndex + 1}
       </h3>
       {<DeleteButton />}
     </header>
